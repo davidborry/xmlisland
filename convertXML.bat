257728fc98169bbe1b2f559dba45511397bb014b
justin <grivon.justin@gmail.com>
2016-05-24 14:32:57 +0200

    update

--- convertXML.bat
+++ convertXML.bat
@@ -1,8 +1,10 @@
-environ=$(uname)
-if [ "$environ" == "Linux" ];then
-    java -cp java-json.jar:out/production/XMLIsland main.java.Main %1
+#!/usr/bin/env bash
 
-else
-    java -cp java-json.jar;out/production/XMLIsland main.java.Main %1
+if [ "$(uname)" == "Darwin" ]; then
+       java -cp java-json.jar:out/production/XMLIsland main.java.Main %1
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+   java -cp java-json.jar:out/production/XMLIsland main.java.Main %1
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+   java -cp java-json.jar;out/production/XMLIsland main.java.Main %1
 fi
 
